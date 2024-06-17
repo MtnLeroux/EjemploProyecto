4f7fe2fb864ac5f668d9ee5c5f423ed1c72ea94c
Leroux <martinleroux2005@gmail.com>
2024-06-17 15:49:20 -0400

    actu 5 (login)

---
+++ resources/views/home/login.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="{{ asset('css/bootstrap-custom.min.css') }}">
+    <title>Sistema: Proyecto Medi</title>
+</head>
+
+<body style="background: linear-gradient(to bottom, #33FFDA 100%, #FFFFFF 0%)">
+    
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/',[HomeController::class,'index'])->name('home.index')->middleware('auth');
+Route::get('/login',[HomeController::class,'login'])->name('home.login');
