cb4396595b79d940adae8394335b3c7a5de5f4a1
Leroux <martinleroux2005@gmail.com>
2024-06-19 21:15:17 -0400

    actu 8.1 (bd)

--- resources/views/home/login.blade.php
+++ resources/views/home/login.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="{{ asset('css/bootstrap-custom.min.css') }}">
-    <title>Sistema: Proyecto Medi</title>
+    <title>Sistema: Proyecto Medicina</title>
 </head>
 
 <body style="background: linear-gradient(to bottom, #ACF6F7 0%, #83D8DA 100%)">
