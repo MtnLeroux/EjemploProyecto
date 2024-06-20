dffdb6309d18c1fd4b24f3ff66152a421f23ce02
Leroux <martinleroux2005@gmail.com>
2024-06-19 21:12:44 -0400

    actu 8

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -10,18 +10,9 @@
         </div>
 
         <div class="row">
-            <!-- equipos -->
+            
             <div class="col-12 col-md-6 col-lg-4 col-xl-2 mb-3">
-                <!--<div class="card">
-                    <img class="card-img-top" src="{{ asset('images/equipos.jpg') }}">
-                    <div class="card-body">
-                        <h5 class="card-title">Equipos</h5>
-                        <div class="btn-group d-flex">
-                            <button class="btn btn-outline-success">Ver</button>
-                            <button class="btn btn-outline-success">Agregar</button>
-                        </div>
-                    </div>-->
-                </div>
+                
             </div>
         </div>
     </div>
--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,11 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\UsuariosController;
 
 
 Route::get('/',[HomeController::class,'index'])->name('home.index');//->middleware('auth');
 Route::get('/login',[HomeController::class,'login'])->name('home.login');
+
+Route::post('usuarios/login',[UsuariosController::class,'login'])->name('usuarios.login');
+Route::post('usuarios/logout',[UsuariosController::class,'logout'])->name('usuarios.logout');
